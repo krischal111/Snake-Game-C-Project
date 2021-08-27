163bd7bddb91e854a17a3bae19411e1af28d13f3
Krischal Khanal <krischal111@gmail.com>
2021-08-27 15:09:05 +0545

    minor correction

--- fhtest.c
+++ fhtest.c
@@ -1,10 +1,13 @@
 #include "Custom Headers\allheaders.h"
-#include "Custom Headers\dataandconstants.h"
-#include "Custom Headers\filehandling.h"
 
+#include "Custom Headers\dataandconstants.h"
 struct makeplayground playground;
 struct makemenudata menudata;
 struct scoredata Best5scores[5];
+
+#include "Custom Headers\filehandling.h"
+
+
 
 int main()
 {
